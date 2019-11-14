490a4ce8bdc3bad3fc76dadb4553b0744c702819
Antony Sanders <antony.devon92@gmail.com>
2019-11-14 19:04:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(sentence)
   new_array = []
   
   sentence.collect do |sentence_reversed|
-    new_array << sentence.split
+    new_array << sentence.split(" ")
   end 
  
   new_array
